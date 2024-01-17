1f0ef47cd29cf785fe49a6cc07484d8cdc218438
Stephen Cronin <stephen.cronin@tulip.co>
2024-01-17 10:25:34 -0500

    Change text on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,6 +54,8 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Find in-depth information about Next.js features and API.
+            <br />
+            Some more text can go here!
           </p>
         </a>
 
